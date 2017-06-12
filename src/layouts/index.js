0c2b2d56d631db0366dfd7854a8ae7dc36028222
Andrey Azov <andrey.azov@gmail.com>
2017-06-12 04:34:11 +0300

    Main page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,18 +1,47 @@
 import PropTypes from 'prop-types';
 import React from "react";
+import Helmet from "react-helmet";
+import { injectGlobal } from 'styled-components';
+
 
 class Layout extends React.Component {
+
   render() {
     return (
-      <div
-        style={{
-          background: 'red'
-        }}
-      >
+      <div>
+        { this.renderHead() }
+        { this.renderBaseStyles() }
         { this.props.children() }
       </div>
     );
   }
+
+  renderHead() {
+    return (
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Andrey Azov: Personal Website</title>
+        <link
+          href='http://fonts.googleapis.com/css?family=PT+Serif&subset=latin,cyrillic,cyrillic-ext,latin-ext'
+          rel='stylesheet'
+          type='text/css'
+        />
+      </Helmet>
+    );
+  }
+
+  renderBaseStyles() {
+    return injectGlobal`
+      html, body {
+        padding: 0;
+        margin: 0;
+      }
+      body {
+        font-size: 62.5%;
+      }
+    `;
+  }
+
 }
 
 Layout.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from "gatsby-link";
 import styled from 'styled-components';
 
-const Test = styled.div`
-  color: yellow;
 
-  span {
-    color: green;
+export default class MainPage extends Component {
+  get title() {
+    const Title = styled.h1`
+      text-align: center;
+      color: #606060;
+      font: 7em "noticia text", serif;
+    `;
+
+    return <Title>Andrey Azov</Title>;
   }
-`;
 
-export default () => (
-  <Test>
-    Hello
-    <Link to="/about/">gatsby</Link>!
-  </Test>
-);
+  get subheader() {
+    const Subheader = styled.h2`
+      text-align: center;
+      color: #606060;
+      font: 4em "noticia text", serif;
+    `;
+
+    return <Subheader>web developer</Subheader>;
+  }
+
+  render() {
+    return (
+      <div>
+        { this.title }
+        { this.subheader }
+      </div>
+    );
+  }
+
+}
